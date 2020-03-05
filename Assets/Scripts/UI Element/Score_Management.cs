bd4a27f6aaa7a1fada21195df72f845bdb6e7ca4
uygwfd254 <uygwfd254@gmail.com>
2020-03-04 21:43:50 -0800

    still working on initialiation of UI

---
+++ Assets/Scripts/UI Element/Score_Management.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Score_Management : Singleton<Score_Management>
+{
+    protected Score_Management() {}
+    // game objects
+    TextMeshProUGUI ScoreText;
+
+    private int level = 0;
+    private int scores = 0;
+    private int lives = 0;
+    private int time = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ScoreText = transform.Find("Score and Info").GetComponent<TextMeshProUGUI>();
+        // original settings
+        level = 1;
+        lives = 3;
+        time = -10;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        update_score_board();
+    }
+
+    void update_score_board() {
+        string information = "Level: " + level.ToString() + "   " +
+                             "Scores: " + scores.ToString() + "   " +
+                             "Lives: " + lives.ToString() + "   " +
+                             "Time: " + time.ToString();
+
+        ScoreText.text = information;
+    }
+}
---
+++ Assets/Scripts/UI Element/UI_Manager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI_Manager : Singleton<UI_Manager>
+{
+    // needed for singleton
+    protected UI_Manager() {}
+
+    // objects
+    RectTransform ParentRectT;
+    RectTransform RandomPipeGridRectT;
+    RectTransform PipeGridRectT;
+    RectTransform ScoreBoardRectT;
+    
+    // object values
+    private float parent_scale = 0f;
+    private Vector2 parent_width_height;
+    private Vector2 random_pipe_grid_width_height;
+
+    // const value
+    private const int RANDOM_PIPE_GRID_ROW = 5;
+    private const int RANDOM_PIPE_GRID_COL = 1;
+
+    void Start()
+    {
+        // objects declaration
+        ParentRectT = GetComponent<RectTransform>();
+        RandomPipeGridRectT = transform.Find("Random Pipe Grid").GetComponent<RectTransform>();
+        PipeGridRectT = transform.Find("Pipe Grid").GetComponent<RectTransform>();
+        ScoreBoardRectT = transform.Find("Score Board").GetComponent<RectTransform>();
+
+        parent_scale = transform.localScale.x;
+    }
+
+    void OnGUI()
+    {
+        parent_width_height = new Vector2(ParentRectT.rect.width,
+                                          ParentRectT.rect.height);
+
+        random_pipe_grid_width_height = new Vector2(RandomPipeGridRectT.rect.width,
+                                                    RandomPipeGridRectT.rect.height);
+    }
+
+    Vector2 get_starting_random_pipe_grid_coords() {
+        Vector2 starting_coords = new Vector2(0f, 0f);
+
+        // formula application
+        starting_coords.x = - (random_pipe_grid_width_height.x / 2);
+        starting_coords.y = (4 * parent_width_height.y - 
+                             random_pipe_grid_width_height.y) / 10;
+        
+        return starting_coords;
+    }
+}
